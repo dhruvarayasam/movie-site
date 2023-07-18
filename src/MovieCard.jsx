7abbcaf4f286d5e914c19fc2d0ef36bf442aaaf3
Dhruva Rayasam <dhruva.rayasam@gmail.com>
2023-07-18 19:05:14 -0400

    added movie title captions to each card

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -5,14 +5,20 @@ const MovieCard = ({ movie }) => {
     // get imdb link
     const imdbLink = `http://imdb.com/title/${movie.imdbID}`
 
-    return (
-        <div className="movie">
-            <div>
-                <p>{movie.Year}</p>
-            </div>
+    console.log(movie)
 
-            <div>
-                <a href={imdbLink} target="_blank" rel="noreferrer"> <img src={movie.Poster !== "N/A" ? movie.Poster : "https://via.placeholder.com/400"} alt={movie.Title} /></a>
+    return (
+        <div>
+            <div className="movie">
+                <div>
+                    <p>{movie.Year}</p>
+                </div>
+                <div>
+                    <a href={imdbLink} target="_blank" rel="noreferrer"> <img src={movie.Poster !== "N/A" ? movie.Poster : "https://via.placeholder.com/400"} alt={'not available'} /></a>
+                </div>
+            </div>
+            <div className="movie-title-container">
+                <a href={imdbLink} target="_blank" rel="noreferrer">{movie.Title}</a>
             </div>
         </div>
     )
