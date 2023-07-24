bfa3f43a4766e404fe6133a1a76efd41e86865cd
Dhruva Rayasam <dhruva.rayasam@gmail.com>
2023-07-23 23:48:50 -0400

    established basics of favorites page

--- src/components/FavoritesPage.js
+++ src/components/FavoritesPage.js
@@ -5,30 +5,166 @@ import { FavoritesContext } from "../contexts/ListContext"
 
 export default function FavoritesPage() {
 
-    const { favoritesList } = useContext(FavoritesContext)
+    const { favoritesList, setFavoritesList } = useContext(FavoritesContext)
 
     // favorites will be organized into folders
     // default is Liked movies
     // can create other folders, but liked will always stay there
     // is organized by favoritesList
-    // each key in favoritesList represents a folder and each value is an array of strings (movie titles)
+    // each key in favoritesList represents a folder and each value is an array of arrays [movie title, imdbID]
     // updates to favoritesList include: appending to a list (folder), deleting from a list, and deleting a folder
+    // can't add any duplicates to a list
 
-    function deleteFromFavoritesList(value) {
+    function deleteFromFolder(folder, imdbID) {
+
+        const listOfMovies = [...favoritesList[folder]]
+
+        for (let i = 0; i < listOfMovies.length; i++) {
+
+            const movie = listOfMovies[i]
+
+            const id = movie[1]
+
+            if (id === imdbID) {
+
+                const newObj = { ...favoritesList }
+
+                newObj[folder].splice(i, 1);
+
+                setFavoritesList({ ...newObj })
+
+                console.log(favoritesList)
+
+
+                return;
+
+            }
+
+        }
+
+        throw new Error('movie not found')
+
 
     }
 
-    function addToFavoritesList(value) {
+    function addToFolder(folder, movieTitle, imdbID) {
+
+        const listOfMovies = [...favoritesList[folder]] // 2D Array
+
+        if (!checkIfMovieAlreadyExists(listOfMovies, imdbID)) { // if movie does not exist in the list, it is safe to add to the list
+
+            const newObj = { ...favoritesList }
+            newObj[folder] = [movieTitle, imdbID]
+
+            // now, newObj is instantiated with all previous data of favoritesList as well as new movie. now, update state
+
+            setFavoritesList({ ...newObj })
+
+        } else {
+
+            throw new Error('movie exists')
+
+        }
+    }
+
+    function addFolder(listName) {
+
+        if (!checkIfFolderAlreadyExists) {
+
+            const objToAdd = {}
+            objToAdd[listName] = []
+
+            setFavoritesList({ ...favoritesList, ...objToAdd })
+
+            console.log(favoritesList)
+
+        } else {
+
+            throw new Error('list already exists')
+
+        }
 
     }
 
-    function deleteList() {
-        
+    function deleteFolder(listName) {
+
+        if (checkIfFolderAlreadyExists(listName)) {
+
+            const newObj = { ...favoritesList }
+
+            delete newObj[listName]
+
+            setFavoritesList({ ...newObj })
+
+            console.log(favoritesList)
+
+
+        } else {
+            return new Error('folder doesnt exist')
+        }
+
     }
 
+    function checkIfMovieAlreadyExists(list, imdbID) {
+
+        for (let i = 0; i < list.length; i++) {
+
+            const movie = list[i]
+
+            const id = movie[1]
+
+            if (id === imdbID) {
+                return false
+            }
+
+        }
+
+        return true
+
+    }
+
+    function checkIfFolderAlreadyExists(listName) {
+
+        return favoritesList.hasOwnProperty(listName)
+
+    }
+
+    // make favoritesList into a renderable array
+
+
+    // now in html, we must iterate through all keys in favoriteList object, assign these as FOLDERS
+    // under each folder, render each movie w/ its respective title 
     return (
         <div>
-            <h1>{favoritesList.test}</h1>
+            <div className="favorites-heading">
+                <h1>FAVORITES</h1>
+            </div>
+            <div className="all-lists">
+                {
+                    Object.entries(favoritesList).map((collection) => {
+                        const folderName = collection[0]
+                        const moviesList = collection[1]
+                        return (
+                            <div className="lists">
+                                <h4>{folderName}</h4>
+                                <div className="movies-folder">
+                                    {
+                                        moviesList.map((movieArr) => {
+                                            const imdbLink = `http://imdb.com/title/${movieArr[1]}`
+                                            const movieTitle = movieArr[0]
+                                            return (
+                                                <div className="movies-folder-object">
+                                                    <a href={imdbLink} target="_blank" rel="noreferrer">{movieTitle}</a>
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div >
         </div>
     )
 }
--- src/contexts/ListContext.js
+++ src/contexts/ListContext.js
@@ -4,7 +4,7 @@ export const FavoritesContext = createContext({})
 
 export function FavoritesListProvider({children}) {
 
-    const [favoritesList, setFavoritesList] = useState({test: 'test value'});
+    const [favoritesList, setFavoritesList] = useState({'all-time-favorites': [['Movie1', 1]], 'list2':[['Mission Impossible Ghost Protocol', 109], ['Harry Potter', 987]]});
 
     return (
         <FavoritesContext.Provider value={{favoritesList, setFavoritesList}}>
