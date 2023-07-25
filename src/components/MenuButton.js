8683786292b8d3d43b56a1dce3315326d983f6eb
Dhruva Rayasam <dhruva.rayasam@gmail.com>
2023-07-25 00:20:16 -0400

    drop down of favoritesList keys

--- src/components/MenuButton.js
+++ src/components/MenuButton.js
@@ -1,16 +1,42 @@
+import { useContext, useState } from 'react'
 import PlusSign from '../plus-sign.svg'
+import { FavoritesContext } from '../contexts/ListContext'
 
 export default function MenuButton(movieTitle, imdbID) {
 
-    function openMenu(ev) {
-        
+    const {favoritesList, setFavoritesList} = useContext(FavoritesContext)
+
+    const [openMenu, setOpenMenu] = useState(false) // if this is true, then menu should be open
+
+    function toggleMenu(ev) {
+
         ev.preventDefault()
 
-        console.log(movieTitle, imdbID)
+        setOpenMenu(!openMenu)
+
     }
 
+    function addToFolder() { // if successfully added to folder, give success alert() message
+
+        console.log('success')
+
+    }
+
+    const folders = Object.keys(favoritesList)
 
     return (
-        <button className="menu-button" onClick={openMenu}><img src={PlusSign} alt='add'></img></button>
+        <div>
+            <button className="menu-button" onClick={toggleMenu}><img src={PlusSign} alt='add'></img></button>
+            {openMenu && (
+                <ul>
+                    {
+                        folders.map((folder) => {
+                            return <li onClick={addToFolder}><button>{folder}</button></li>
+                        })
+                    }
+                </ul>
+            )}
+        </div>
+
     )
 }
